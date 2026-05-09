0677e79aae04ef9f130ca0fadef1b24e6b11e68c
UIT.25520771 <25520771@gm.uit.edu.vn>
2026-05-09 23:13:16 +0700

    Update Tetris UI

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
-#include <ctime>
 using namespace std;
 
-#define H 20
-#define GAME_W 40
+#define H 30
+#define GAME_W 60
 
-#define OFFSET_X 20
-#define OFFSET_Y 2
+int OFFSET_X;
+int OFFSET_Y;
 
 char board[H][GAME_W] = {};
 
@@ -24,7 +23,7 @@ char blocks[][4][4] = {
     {
         {' ',' ',' ',' '},
         {'I','I','I','I'},
-        {' ',' ',' ',' '},      
+        {' ',' ',' ',' '},
         {' ',' ',' ',' '}
     },
 
@@ -168,48 +167,6 @@ bool canMove(int dx, int dy){
     return true;
 }
 
-void rotateBlock() {
-    boardDelBlock(); 
-    char rotated[4][4]; 
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 4; j++)
-            rotated[j][3 - i] = blocks[b][i][j];
-
-    
-    int kick[] = { 0, 1, -1, 2, -2 };
-    int newX = x;
-    bool canRotate = false;
-
-    for (int k = 0; k < 5 && !canRotate; k++) {
-        bool ok = true;
-        int testX = x + kick[k];
-
-        for (int i = 0; i < 4 && ok; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (rotated[i][j] != ' ') {
-                    int tx = testX + j;
-                    int ty = y + i;
-                    
-                    if (tx < 2 || tx >= GAME_W - 2 || ty >= H - 1 || board[ty][tx] != ' ') {
-                        ok = false;
-                        break;
-                    }
-                }
-            }
-        }
-        if (ok) {
-            canRotate = true;
-            newX = testX;
-        }
-    }
-
-    if (canRotate) {
-        x = newX;
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
-                blocks[b][i][j] = rotated[i][j];
-    }
-}
 void draw();
 
 void removeLine(){
@@ -314,8 +271,11 @@ void draw(){
 
         for (int j = 0 ; j < GAME_W ; j++){
 
-            if (board[i][j] == ' ')
+            if (board[i][j] == ' ') {
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
                 cout << '.';
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+            }
             else
                 cout << board[i][j];
         }
@@ -329,81 +289,168 @@ void drawMenu(){
     system("cls");
     system("color 0A");
 
-    cout << R"(
+    string logo[] = {
 
-        ,----,                 ,----,
-      ,/   .`|               ,/   .`|
-    ,`   .'  :   ,---,.    ,`   .'  :,-.----.     ,---,  .--.--.
-  ;    ;     / ,'  .' |  ;    ;     /\    /  \ ,`--.' | /  /    '.
-.'___,/    ,',---.'   |.'___,/    ,' ;   :    \|   :  :|  :  /`. /
-|    :     | |   |   .'|    :     |  |   | .\ ::   |  ';  |  |--`
-;    |.';  ; :   :  |-,;    |.';  ;  .   : |: ||   :  ||  :  ;_
-`----'  |  | :   |  ;/|`----'  |  |  |   |  \ :'   '  ; \  \    `.
-    '   :  ; |   :   .'    '   :  ;  |   : .  /|   |  |  `----.   \
-    |   |  ' |   |  |-,    |   |  '  ;   | |  \'   :  ;  __ \  \  |
-    '   :  | '   :  ;/|    '   :  |  |   | ;\  \   |  ' /  /`--'  /
-    ;   |.'  |   |    \    ;   |.'   :   ' | \.'   :  |'--'.     /
-    '---'    |   :   .'    '---'     :   : :-' ;   |.'   `--'---'
-             |   | ,'                |   |.'   '---'
-             `----'                  `---'
+"        ,----,                 ,----,",
+"      ,/   .`|               ,/   .`|",
+"    ,`   .'  :   ,---,.    ,`   .'  :,-.----.     ,---,  .--.--.",
+"  ;    ;     / ,'  .' |  ;    ;     /\\    /  \\ ,`--.' | /  /    '.",
+".'___,/    ,',---.'   |.'___,/    ,' ;   :    \\|   :  :|  :  /`. /",
+"|    :     | |   |   .'|    :     |  |   | .\\ ::   |  ';  |  |--`",
+";    |.';  ; :   :  |-,;    |.';  ;  .   : |: ||   :  ||  :  ;_",
+"`----'  |  | :   |  ;/|`----'  |  |  |   |  \\ :'   '  ; \\  \\    `.",
+"    '   :  ; |   :   .'    '   :  ;  |   : .  /|   |  |  `----.   \\",
+"    |   |  ' |   |  |-,    |   |  '  ;   | |  \\'   :  ;  __ \\  \\  |",
+"    '   :  | '   :  ;/|    '   :  |  |   | ;\\  \\   |  ' /  /`--'  /",
+"    ;   |.'  |   |    \\    ;   |.'   :   ' | \\.'   :  |'--'.     /",
+"    '---'    |   :   .'    '---'     :   : :-' ;   |.'   `--'---'",
+"             |   | ,'                |   |.'   '---'",
+"             `----'                  `---'"
+    };
 
-)";
+    int longest = 0;
 
-    cout << "\n";
+    for (int i = 0 ; i < 15 ; i++){
 
+        if (logo[i].length() > longest)
+            longest = logo[i].length();
+    }
 
-    cout << "                 >> START GAME\n";
-    cout << "                    HIGH SCORE : " << highScore << "\n";
-    cout << "                    PRESS ENTER\n\n";
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
 
-    cout << "          ╔════════ CONTROLS ═══════════╗\n";
-    cout << "          ║  [A] DI CHUYEN QUA TRAI     ║\n";
-    cout << "          ║  [D] DI CHUYEN QUA PHAI     ║\n";
-    cout << "          ║  [X] DI CHUYEN XUONG DUOI   ║\n";
-    cout << "          ║  [W] XOAY                   ║\n";
-    cout << "          ║  [Q] THOAT GAME             ║\n";
-    cout << "          ╚═════════════════════════════╝\n";
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+
+    int consoleWidth =
+        csbi.srWindow.Right - csbi.srWindow.Left + 1;
+
+    int startX = (consoleWidth / 2) - 30;
+
+    int y = 2;
+
+    for (int i = 0 ; i < 15 ; i++){
+
+        gotoxy(startX - 7, y++);
+        cout << logo[i];
+    }
+
+    gotoxy(startX + 17, y + 2);
+    cout << ">> START GAME";
+
+    gotoxy(startX + 20, y + 4);
+    cout << "HIGH SCORE : " << highScore;
+
+    gotoxy(startX + 20, y + 6);
+    cout << "PRESS ENTER";
+
+    gotoxy(startX + 10, y + 10);
+    cout << "╔════════ CONTROLS ═══════════╗";
+
+    gotoxy(startX + 10, y + 11);
+    cout << "║  [A] DI CHUYEN QUA TRAI     ║";
+
+    gotoxy(startX + 10, y + 12);
+    cout << "║  [D] DI CHUYEN QUA PHAI     ║";
+
+    gotoxy(startX + 10, y + 13);
+    cout << "║  [X] DI CHUYEN XUONG DUOI   ║";
+
+    gotoxy(startX + 10, y + 14);
+    cout << "║  [Q] THOAT GAME             ║";
+
+    gotoxy(startX + 10, y + 15);
+    cout << "╚═════════════════════════════╝";
+}
+
+void centerGame(){
+
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+    GetConsoleScreenBufferInfo(
+        GetStdHandle(STD_OUTPUT_HANDLE),
+        &csbi
+    );
+
+    int consoleWidth =
+        csbi.srWindow.Right - csbi.srWindow.Left + 1;
+
+    int consoleHeight =
+        csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+
+    int totalWidth = GAME_W + 30;
+
+    OFFSET_X = (consoleWidth - totalWidth) / 2;
+
+    OFFSET_Y = (consoleHeight - H) / 2;
 }
 
 int main(){
+
     system("chcp 65001 > nul");
+
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
+
+    system("mode con cols=140 lines=40");
+    system("cls");
+    centerGame();
+    system("color 0A");
+
     srand(time(0));
 
     drawMenu();
+
     while (getch() != 13);
+
     system("cls");
 
-    initBoard();
     b = rand() % 8;
     nextBlock = rand() % 8;
 
+    initBoard();
+
     while (1){
+
         boardDelBlock();
+
         if (kbhit()){
+
             char c = getch();
-            if (c == 'a' && canMove(-1,0)) x--;
-            if (c == 'd' && canMove(1,0)) x++;
-            if (c == 'x' && canMove(0,1)) y++;
-            if (c == 'w') rotateBlock(); 
-            if (c == 'q') break;
+
+            if (c == 'a' && canMove(-1,0))
+                x--;
+
+            if (c == 'd' && canMove(1,0))
+                x++;
+
+            if (c == 'x' && canMove(0,1))
+                y++;
+
+            if (c == 'q')
+                break;
         }
 
-        if (canMove(0,1)) y++;
-        else {
+        if (canMove(0,1))
+            y++;
+
+        else{
+
             block2Board();
+
             removeLine();
+
             x = (GAME_W - 2) / 2;
             y = 0;
+
             b = nextBlock;
             nextBlock = rand() % 8;
-            if (!canMove(0,0)) break; 
         }
 
         block2Board();
+
         draw();
+
         Sleep(max(50, 200 - level * 10));
     }
+
     return 0;
 }
